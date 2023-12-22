026c042c13a1fd857447d6a215f204490c345972
Meena R <154489214+innovate444444@users.noreply.github.com>
2023-12-22 22:39:59 +0530

    m

--- formula.py
+++ formula.py
@@ -1,4 +1,3 @@
-
 def formula(ver_h, ver_t, hr_h, hr_t, det_t):
     no_of_units = det_t/(ver_t+hr_t)
     hr_comp = int(str(no_of_units)[0])
@@ -11,9 +10,21 @@ def formula(ver_h, ver_t, hr_h, hr_t, det_t):
         depth = ver_tr
     
     return (hr_tr, depth)
+detected_cracks = [
+    "frames/frame_00000090.png",
+    "frames/frame_00000100.png",
+    "frames/frame_00000235.png",
+    # Add more paths as needed
+]
 
-det=[700,908,2098,3000]
-for i in det:
+# Extracting last two digits from each path and storing them in det_t list
+det_t = [int(path[-7:-4]) for path in detected_cracks]
+
+print(det_t)
+  # Displaying the extracted digits
+
+
+for i in det_t:
     ver_h,ver_t,hr_h,hr_t=20,180,1,5
     l1=formula(ver_h,ver_t,hr_h,hr_t,i)
 
